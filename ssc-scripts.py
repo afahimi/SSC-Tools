bfeea97bddbe47a602c7e4ad1c17adf5a24c31fc
afahimi <amin.f1000@gmail.com>
2023-05-15 00:19:25 -0700

    emailing

--- ssc-scripts.py
+++ ssc-scripts.py
@@ -8,6 +8,67 @@ import getpass
 from threading import Thread
 import smtplib
 from info import info
+from flask import Flask
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+def sendEmail(course: str, grade: str, email: str) -> None:
+    # Set up the browser
+    browser = webdriver.Chrome('C:/Users/Feridoun/Documents/Personal Projects/chromedriver.exe')
+    browser.get('https://mail.protonmail.com/login')
+
+    time.sleep(5)
+
+    # Log in
+    email_input = browser.find_element(By.CLASS_NAME, 'field-two-container field-two--bigger')
+    email_input.send_keys('ssc.scripts@proton.me')
+    password_input = browser.find_element_by_name('password')
+    password_input.send_keys('ssc.scripts123123')
+    password_input.send_keys(Keys.RETURN)
+
+    # Compose the email
+    compose_button = browser.find_element_by_xpath('//div[contains(text(), "Compose")]')
+    compose_button.click()
+    to_input = browser.find_element_by_name('to')
+    to_input.send_keys(email)
+    subject_input = browser.find_element_by_name('subject')
+    subject_input.send_keys(f'Grade Notification for {course}')
+    body_input = browser.find_element_by_xpath('//div[@aria-label="Message Body"]')
+    body_input.send_keys(f'Dear Student,\n\nYour grade for the course {course} has been released. Your grade is: {grade}.\n\nThank you.')
+    send_button = browser.find_element_by_xpath('//button[@title="Send"]')
+    send_button.click()
+
+    # Close the browser
+    browser.quit()
+
+# def sendEmail(course: str, grade: str, email: str) -> None:
+#     sender_email = 'ssc.scripts@proton.me'
+#     sender_password = 'ssc.scripts123123'
+#     recipient_email = email
+
+#     subject = f'Grade Notification for {course}'
+#     message = f'Dear Student,\n\nYour grade for the course {course} has been released. Your grade is: {grade}.\n\nThank you.'
+
+#     # Create a multipart message and set headers
+#     email = MIMEMultipart()
+#     email['From'] = sender_email
+#     email['To'] = recipient_email
+#     email['Subject'] = subject
+
+#     # Add the message body to the email
+#     email.attach(MIMEText(message, 'plain'))
+
+#     # Send the email
+#     try:
+#         server = smtplib.SMTP('smtp.gmail.com', 587)
+#         server.starttls()
+#         server.login(sender_email, sender_password)
+#         server.send_message(email)
+#         print(f'Email sent successfully to {recipient_email}.')
+#     except smtplib.SMTPException as e:
+#         print(f'Error sending email to {recipient_email}: {e}')
+
+
 
 
 
@@ -33,6 +94,12 @@ def checkGrade(driver: webdriver, grades: list[int]) -> None:
         time.sleep(1)
 
 
+# Usage example
+course = 'Mathematics'
+grade = 'A'
+recipient_email = 'amin.f1000@gmail.com'
+sendEmail(course, grade, recipient_email)
+
 
 if __name__ == '__main__':
     username = info.username
@@ -40,7 +107,7 @@ if __name__ == '__main__':
     grades = info.grades
     email = info.email
     # Open the browser and go to the SSC login page
-    driver = webdriver.Chrome('./chromedriver.exe')
+    driver = webdriver.Chrome('C:/Users/Feridoun/Documents/Personal Projects/chromedriver.exe')
     #driver.get('https://ssc.adm.ubc.ca/')
     driver.get("https://cas.id.ubc.ca/ubc-cas/login?TARGET=https%3A%2F%2Fssc.adm.ubc.ca%2Fsscportal%2Fservlets%2FSSCMain.jsp%3Ffunction%3DSessGradeRpt")
     # element = driver.find_element(By.CLASS_NAME, 'span6')
@@ -70,8 +137,14 @@ if __name__ == '__main__':
     # WebDriverWait(driver, timeout=15).until(
     # EC.frame_to_be_available_and_switch_to_it("iframe-main"))
 
-    t1 = Thread(target=checkGrade, args=(driver, "CPEN 212"))
-    t1.start()
+    # t1 = Thread(target=checkGrade, args=(driver, "CPEN 212"))
+    # t1.start()
+
+    # Usage example
+    # course = 'Mathematics'
+    # grade = 'A'
+    # recipient_email = 'amin.f1000@gmail.com'
+    # sendEmail(course, grade, recipient_email)
 
     
 
